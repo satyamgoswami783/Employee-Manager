2e44c00e83cd7ff09c4a2a3ffc8503eae08c08cf
satyamgoswami783 <sg2467910@gmail.com>
2026-01-30 08:40:13 +0530

    Configure Backend CORS for Frontend URL & verify Frontend API URL

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({ origin: 'https://employee-manager-iban.onrender.com', credentials: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
